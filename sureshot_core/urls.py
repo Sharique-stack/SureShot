d1ef74cb50e26c52ff9306a6b132e6caf8241deb
Mohammad Sharique <sharique1997@gmail.com>
2026-06-18 18:16:24 +0530

    Fix: Imported 'include' for auth urls

--- sureshot_core/urls.py
+++ sureshot_core/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from django.views.generic import RedirectView
 from tracker import views
